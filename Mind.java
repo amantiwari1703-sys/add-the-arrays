674720987814320068ee802f69b869dbbd162766
Aman Tiwari <amantiwari@gmail.com>
2026-04-18 21:15:12 +0530

    add

--- Mind.java
+++ Mind.java
@@ -1,15 +1,10 @@
 public class Mind {
-    static int sum(int n) {
-        int s = 0;
-        for (int i = 1; i <= n; i++) {
-            s += i;
-        }
-        return s;
-
-
-        }
-    public static void main(String args[]){
-        System.out.println(sum(5));
-
-    }
+  public static void main(String[] args){
+      int[]arr={1,2,3};
+      int sum=0;
+      for(int x:arr){
+          sum+=x;
+      }
+      System.out.println(sum);
+  }
 }
