494e2d8f5a2a4c494ee95b06ee9335ed19ff9e88
Aman Tiwari <amantiwari@gmail.com>
2026-04-18 21:43:48 +0530

    reverse

--- Mind.java
+++ Mind.java
@@ -1,10 +1,8 @@
 public class Mind {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3,4};
-        for (int x : arr) {
-            if (x % 2 == 0) {
-                System.out.println(x);
-            }
-        }
+     int[]arr={1,2,3};
+     for(int i=arr.length-1;i>=0;i--){
+         System.out.println(arr[i]);
+     }
+     }
     }
-}
